67994a607ab517af8f75124e6930b8a56cbeb1ec
marianonajeny <119488816+marianonajeny@users.noreply.github.com>
2022-11-30 01:23:52 -0800

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,6 @@
+document.writeln ("<center>");
+document.writeln("<hr width=600 color=black>");
+document.writeln("<h1>DATA MAHASISWA</h1>");
+document.writeln("<h2>KAMPUS POLITEKNIK AMAMAPARE</h2>");
+document.writeln("<hr width=600 color=black>");
+document.writeln ("</center>");
